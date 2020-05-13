960d6ad5837b6ce081c394e2c5d23ef8f290abae
Samuel <samuelpp.s@hotmail.com>
2020-05-13 15:26:39 -0300

    exercicio7

---
+++ exercises/4-2/exercicio7.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let c = numbers[0];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (c > numbers[i]) {
+        c = numbers[i]
+    } else {
+        c = c
+    }
+}
+console.log(c)
