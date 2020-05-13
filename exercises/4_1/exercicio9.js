ea016e10d86884bed94c76a447e898daca2fd6c8
Samuel <samuelpp.s@hotmail.com>
2020-05-13 12:17:19 -0300

    exercicio 9

---
+++ exercises/4_1/exercicio9.js
@@ -0,0 +1,9 @@
+let a = 4, b = 6, c = 15, PI = (a + b) % 2; resto = (PI + c) % 2, resultado = false;
+
+if (PI === 1 || resto === 1) {
+    resultado = true;
+    console.log(resultado)
+} else {
+    console.log(resultado)
+}
+ 
