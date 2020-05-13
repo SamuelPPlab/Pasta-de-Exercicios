71dca91c1b4939fd1cca2ae4791abbfdb3dfcd2f
Samuel <samuelpp.s@hotmail.com>
2020-05-13 07:26:43 -0300

    exercicio 1

---
+++ exercises/4_1/exercicio1.js
@@ -0,0 +1,27 @@
+let a = 9;
+let b = 10 ;
+
+//adição
+let c = a + b;
+
+console.log(c);
+
+//subtração
+let d = a - b;
+
+console.log(d);
+
+//multiplicação
+let e = a * b;
+
+console.log(e);
+
+//divisão
+let f = a / b; 
+
+console.log(f);
+
+//módulo
+let g = a % b;
+
+console.log(g);
