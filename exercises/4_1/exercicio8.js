27b16a73bba931c26ea4bb305ff959f4a685c941
Samuel <samuelpp.s@hotmail.com>
2020-05-13 10:40:50 -0300

    exercicio 8

---
+++ exercises/4_1/exercicio8.js
@@ -0,0 +1,9 @@
+let a = 10, b = 5, c = 3, resto = (a + b + c) % 2, resultado = false;
+
+if (resto === 0) {
+    resultado = true;
+    console.log(resultado)
+} else {
+    console.log(resultado)
+}
+ 
