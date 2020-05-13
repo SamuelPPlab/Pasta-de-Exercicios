b333333f3b40c84e50c8b81ecf11caf2dc9aeb9e
Samuel <samuelpp.s@hotmail.com>
2020-05-13 12:28:46 -0300

    exercicio 10

---
+++ exercises/4_1/exercicio10.js
@@ -0,0 +1,7 @@
+let custo = 500, venda = 1000, lucro = venda - custo;
+
+if (custo < 0 || venda < 0 || lucro < 0) {
+    console.log("Você está devendo.")
+} else {
+console.log((lucro - (lucro * 0.2)) * 1000)
+}
