2040786b8c4a376a6c4ce365fb9cbd2aedb0a2d4
Samuel <samuelpp.s@hotmail.com>
2020-05-13 10:24:26 -0300

    exercicio 7

---
+++ exercises/4_1/exercicio7.js
@@ -0,0 +1,15 @@
+let nota = 25;
+
+if (nota <= 50) {
+    console.log("Você tirou um F. :(")
+} else if (nota<= 60) {
+    console.log("Você tirou um E. :(")
+} else if (nota<= 70) {
+    console.log("Você tirou um D. :|")
+} else if (nota<= 80) {
+    console.log("Você tirou um C. :}")
+} else if (nota<= 90) {
+    console.log("Você tirou um B. :]")
+} else {
+    console.log("Você tirou um A. XD")
+}
