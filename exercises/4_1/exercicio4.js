f1c701e58c76a752211ae03c1635526f35d0f5ae
Samuel <samuelpp.s@hotmail.com>
2020-05-15 09:57:09 -0300

    exercicio 4 virou função também

--- exercises/4_1/exercicio4.js
+++ exercises/4_1/exercicio4.js
@@ -1,9 +1,9 @@
-let a = -10;
-
+function posNegZer (a) {
 if (a === 0) {
     console.log("É zero.");
 } else if (a > 0) {
     console.log ("É positivo.")
 } else {
     console.log ("É negativo")
+}
 }
