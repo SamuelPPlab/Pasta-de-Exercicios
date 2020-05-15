234075aa7de191dc579b227487ef62c826eefe09
Samuel <samuelpp.s@hotmail.com>
2020-05-15 09:55:12 -0300

    exercicio 3 é agora uma função.

--- exercises/4_1/exercicio3.js
+++ exercises/4_1/exercicio3.js
@@ -1,13 +1,14 @@
-let a = 14, b = 17, c = 2, comp = a;
+function comaparadorDe3 (a, b, c) {
+    let comp = undefined;
+    if (a > b) {
+        comp = a;
+    } else {
+        comp = b
+    }
 
-if (a > b) {
-    comp = a;
-} else {
-    comp = b
+    if(c > comp) {
+        console.log(c);
+    } else {
+        console.log(comp);
 }
-
-if(c > comp) {
-    console.log(c);
-} else {
-    console.log(comp);
 }
