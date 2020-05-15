93e569c1c554268bea1355a26abf345f2d82c7cd
Samuel <samuelpp.s@hotmail.com>
2020-05-15 09:59:17 -0300

    o 5 também virou função

--- exercises/4_1/exercicio5.js
+++ exercises/4_1/exercicio5.js
@@ -1,7 +1,8 @@
-let a1 = 60, a2 = 60, a3 = 60, s3 = a1 + a2 + a3;
-
+function triangle (a1, a2, a3) {
+let s3 = a1 + a2 + a3;
 if (s3 === 180) {
     console.log("É um triângulo.");
 } else {
     console.log("Não é um triângulo.")
+}
 }
