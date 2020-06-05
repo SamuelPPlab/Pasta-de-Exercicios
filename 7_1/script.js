4dd6079b16aa1c8ca95eff0177b825dc50bb9a57
SamuelPPlab <samuelpp.s@hotmail.com>
2020-06-05 18:29:23 -0300

    exercicio 2 feito.

---
+++ 7_1/script.js
@@ -0,0 +1,27 @@
+//Exercicio 1
+function fatorial(n) {
+  let soma = 1;
+  for (let i = 2; i <= n; i += 1) soma = soma * i;
+  return soma;
+}
+console.log(fatorial(5));
+
+//Exercicio 2
+
+function maiorPalavra (array) {
+  let an = 0;
+  let mp = 0;
+  const arrayS = array.split(' ');
+  for (let i = 0; i < arrayS.length; i ++) {
+    const pd = arrayS[i].split('');
+    let t = pd.length;
+  if(t > mp) {
+    mp = t; 
+    an = arrayS[i];
+  }
+  }
+  return(an);
+  }
+  console.log(maiorPalavra("Antônio foi no banheiro e não sabemos o que aconteceu"))
+
+
