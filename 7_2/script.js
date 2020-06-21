c1f024abcf933cf1db74121a05be8c8aa13521d7
SamuelPPlab <samuelpp.s@hotmail.com>
2020-06-21 03:47:09 -0300

    exercicio 1 ok

---
+++ 7_2/script.js
@@ -0,0 +1,25 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+//Exercicio 1 - adicionar uma função com 3 parametros que adiciona um turno na lesson2
+
+function mod (alvo, chave, valor) {
+  alvo[chave] = valor;
+}
