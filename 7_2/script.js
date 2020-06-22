b3adb5028ec24e0a08851dd11e9aa994fb644ccc
SamuelPPlab <samuelpp.s@hotmail.com>
2020-06-21 21:07:07 -0300

    exercicio 9 pronto.

--- 7_2/script.js
+++ 7_2/script.js
@@ -77,3 +77,15 @@ function placeFinderK (obj, index) {
   const keys = Object.keys(obj);
   return(keys[index]);
 }
+
+//Exercicio 9 criar uma função que procura dados num objeto.
+
+function lupa (obj, key, value) {
+  const comparado1 = Object.entries(obj);
+  let resultado = false;
+  for (i in comparado1) {
+    if (comparado1[i][0] === key && comparado1[i][1] === value) resultado = true;
+  }
+  return (resultado)
+}
+console.log(lupa(lesson3, 'turnor', 'noite'))
