1753f83cdfe0fab5ea9936cf9e6d0b56bac934ca
SamuelPPlab <samuelpp.s@hotmail.com>
2020-06-24 23:10:14 -0300

    exercicios 3

--- 7_4/script.js
+++ 7_4/script.js
@@ -18,3 +18,23 @@ const output = addOne(myArray);
 assert.strictEqual(typeof addOne, 'function');
 assert.deepEqual(output, expected);
 assert.deepEqual(myArray, unchanged);
+
+//Exercicio 2
+
+const assert = require('assert');
+
+function wordLengths (arr) {
+  const array = [];
+  for (let i = 0; i < arr. length; i++) {
+    const palarvra = arr[i].split('');
+    array.push(palarvra.length);
+  }
+  return(array);
+}
+
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
+assert.deepEqual(output, expected);
